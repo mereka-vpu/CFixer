1e9f58d7ec952ac4b001216db6f51e09060b4e66
mereka-vpu <80886830+mereka-vpu@users.noreply.github.com>
2025-02-24 15:31:00 +0700

    Shpot

--- src/main/java/nzst/laminan/FalconForce.java
+++ src/main/java/nzst/laminan/FalconForce.java
@@ -7,7 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import nzst.laminan.falconforce.CommandBlocker;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -16,68 +15,72 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashSet;
-import java.util.Properties;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class FalconForce extends JavaPlugin implements CommandExecutor {
-    private final Set<UUID> verifiedPlayers = new HashSet<>();
+    private final Map<String, Boolean> loggedIn = new HashMap<>();
+    private final Map<String, String> pendingEmails = new HashMap<>();
 
     @Override
     public void onEnable() {
-        getCommand("facon").setExecutor(this);
-        getCommand("login").setExecutor(this);
-        getServer().getPluginManager().registerEvents(new CommandBlocker(this), this);
+        this.getCommand("login").setExecutor(this);
+        this.getCommand("facon").setExecutor(this);
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player) && !command.getName().equalsIgnoreCase("faconaprolog")) {
-            sender.sendMessage(ChatColor.RED + "Unknown command.");
+        if (!(sender instanceof Player)) return false;
+        Player player = (Player) sender;
+        
+        if (command.getName().equalsIgnoreCase("login")) {
+            if (loggedIn.getOrDefault(player.getName(), false)) {
+                player.sendMessage(ChatColor.RED + "You are already logged in.");
+                return true;
+            }
+            player.sendMessage(ChatColor.GREEN + "You need to use /facon to verify your login.");
             return true;
         }
 
         if (command.getName().equalsIgnoreCase("facon")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                String email = fetchEmail(player.getName());
-                if (email == null) {
-                    player.sendMessage(ChatColor.RED + "Error fetching your email. Contact admin.");
-                    return true;
-                }
-                
-                String maskedEmail = maskEmail(email);
-                player.sendMessage(ChatColor.GREEN + "We sent you an email to " + maskedEmail);
-                sendVerificationEmail(player, email);
-            }
-        } else if (command.getName().equalsIgnoreCase("faconaprolog")) {
-            if (args.length < 2) {
-                sender.sendMessage(ChatColor.RED + "Usage: faconaprolog <username> <code>");
+            if (loggedIn.getOrDefault(player.getName(), false)) {
+                player.sendMessage(ChatColor.RED + "You are already logged in.");
                 return true;
             }
             
-            String username = args[0];
-            String code = args[1];
-            Player player = Bukkit.getPlayer(username);
-            if (player != null) {
-                verifyPlayer(player);
+            String email = getEmailFromAPI(player.getName());
+            if (email == null) {
+                player.sendMessage(ChatColor.RED + "Could not find registered email.");
+                return true;
             }
+            
+            pendingEmails.put(player.getName(), email);
+            sendVerificationEmail(player, email);
+            player.sendMessage(ChatColor.YELLOW + "We sent an email to " + maskEmail(email));
         }
         return true;
     }
-
-    public void verifyPlayer(Player player) {
-        verifiedPlayers.add(player.getUniqueId());
-        player.sendMessage(ChatColor.GREEN + "You are now verified!");
+    
+    public void verifyPlayer(String username) {
+        Player player = Bukkit.getPlayer(username);
+        if (player != null) {
+            loggedIn.put(username, true);
+            player.sendMessage(ChatColor.GREEN + "Login approved. You may now use commands.");
+        }
+    }
+    
+    public void kickPlayer(String username) {
+        Player player = Bukkit.getPlayer(username);
+        if (player != null) {
+            player.kickPlayer(ChatColor.RED + "Login denied.");
+        }
     }
 
-    private String fetchEmail(String username) {
+    private String getEmailFromAPI(String username) {
         try {
-            URL url = new URL("https://infra1.laminan.my.id/getemail.php?username=" + username);
+            URL url = new URL("http://infra1.laminan.my.id/getemail.php?username=" + username);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String email = in.readLine();
             in.close();
@@ -88,40 +91,39 @@ public class FalconForce extends JavaPlugin implements CommandExecutor {
     }
 
     private void sendVerificationEmail(Player player, String email) {
-        String verificationLink = "https://infra1.laminan.my.id/verify?user=" + player.getUniqueId();
-        String message = "Is this you? Click below to verify:\n" + verificationLink;
+        String username = player.getName();
+        String subject = "Minecraft Login Verification";
+        String body = "Is this you logging in? <a href='http://infra1.laminan.my.id/verify?user=" + username + "&approve=true'>Yes</a> " +
+                      "<a href='http://infra1.laminan.my.id/verify?user=" + username + "&approve=false'>No</a>";
         
-        final String username = "noreply@builderzun.serv00.net";
-        final String password = "Arkanataya321";
-
         Properties props = new Properties();
+        props.put("mail.smtp.host", "builderzun.serv00.net");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "builderzun.serv00.net");
-        props.put("mail.smtp.port", "587");
-
+        
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
+                return new PasswordAuthentication("noreply@builderzun.serv00.net", "Arkanataya321");
             }
         });
-
+        
         try {
-            Message mimeMessage = new MimeMessage(session);
-            mimeMessage.setFrom(new InternetAddress(username));
-            mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-            mimeMessage.setSubject("FalconForce Verification");
-            mimeMessage.setText(message);
-
-            Transport.send(mimeMessage);
-        } catch (MessagingException e) {
-            e.printStackTrace();
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("noreply@builderzun.serv00.net"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject(subject);
+            message.setContent(body, "text/html");
+            Transport.send(message);
+        } catch (Exception e) {
+            player.sendMessage(ChatColor.RED + "Failed to send email.");
         }
     }
 
     private String maskEmail(String email) {
-        int atIndex = email.indexOf('@');
-        if (atIndex <= 1) return "****" + email.substring(atIndex);
-        return email.charAt(0) + "****" + email.substring(atIndex);
+        int atIndex = email.indexOf("@");
+        if (atIndex > 1) {
+            return email.charAt(0) + "****" + email.substring(atIndex);
+        }
+        return "****@****";
     }
 }
